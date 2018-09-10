afa64bdf325a7d2bb56c50c0181cd6b75d281a26
Jim Ladd <jladd@redhat.com>
2018-09-10 06:04:55 -0700

    Initial commit

---
+++ script.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import os
+import time
+
+from slackclient import SlackClient
+
+channels = [('channel-name', 'C0000000000')]
+
+slack_token = os.environ["SLACK_TOKEN"]
+sc = SlackClient(slack_token)
+
+def check_channel(channel):
+    channel_name, channel_id = channel
+    print('Reading {}'.format(channel_name))
+
+    conv_history = sc.api_call("channels.history", channel=channel_id)
+    #print conv_history.keys()
+    #import pdb; pdb.set_trace()
+
+    timestamps = []
+    backoff_period = 0
+    current_time = time.time()
+    for msg in reversed(conv_history['messages']):
+        timestamp = float(msg['ts'])
+
+        if current_time - timestamp > 60 * 5:
+            continue
+
+        timestamps.append(timestamp)
+
+        if len(timestamps) < 10:
+            continue
+
+        if (timestamp > backoff_period) and \
+           (timestamp - timestamps[0] < 30 *  9):
+            avg_rate = (timestamp - timestamps[0]) / 9.0
+            backoff_period = timestamp + 180
+            timestamp_str = time.strftime('%H:%M:%S', time.localtime(timestamp))
+            notice = u"""In <#{0}|{1}>, at {2}, "{3}" (_rate: {4} sec/msg_)""".format(channel_id, channel_name, timestamp_str, msg['text'], int(avg_rate))
+
+            print(notice)
+            sc.api_call(
+              "chat.postMessage",
+              channel="D1EPGDKGB", # jim
+              text=notice
+            )
+            time.sleep(5)
+
+        timestamps.pop(0)
+
+while True:
+    for channel in channels:
+        check_channel(channel)
+        time.sleep(5)
+    time.sleep(60 * 5)
+
