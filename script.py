dd6bdd377ec11d93d1739344f401d4628ca13e86
Jim Ladd <jladd@redhat.com>
2019-05-10 17:53:23 -0700

    Finished refactor

--- script.py
+++ script.py
@@ -6,12 +6,14 @@ import time
 
 from slackclient import SlackClient
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('tldrbot')
+logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-logger.addHandler(ch)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 #channels = [('channel_name', 'C0000000000')]
 #users = [('user_name', 'U0000000000')]
 
@@ -45,75 +47,107 @@ class SlackChannel(object):
         logger.debug(u'Calling {0} on {1} ({2})'.format(api_method, self.channel_id, params))
         return sc.api_call(api_method, **params)
 
+
+class ScoredMessage(object):
+
+    def __init__(self, score, msg, callback):
+        self.score = 0
+        self.msg = msg
+        self.callback = callback
+
+    def upvote(self):
+        self.score += 1
+        self.callback(self.score, self.msg)
+
+
+class ScoredMessages(object):
+
+    def __init__(self, threshold=1, max_length=500, callbacks=None):
+        self.messages = []
+        self.threshold = threshold
+        self.max_length = max_length
+        self.last_read = -1
+        self.callbacks = callbacks if type(callbacks) is list else [callbacks]
+
+    def __str__(self):
+        return u'\n'.join(['{0}\t{1:125.125}'.format(m.score, m.msg['text']) for m in self.messages])
+
+    def extend(self, msgs):
+        msgs = msgs if type(msgs) is list else list(msgs)
+        # increase buffer if necessary
+        if len(msgs) > self.max_length:
+            self.max_length = len(msgs)
+
+        self.last_read = len(self.messages) - 1
+
+        def _upvote_hook(new_score, msg):
+            if new_score == self.threshold:
+                for cb in self.callbacks:
+                    cb(msg)
+        self.messages.extend([ScoredMessage(0, msg, _upvote_hook) for msg in msgs])
+
+        if len(self.messages) > self.max_length:
+            self.last_read -= len(self.messages) - len(self.max_length)
+            self.messages = self.messages[-self.max_length:]
+
+    def getCurrentBatch(self):
+        return self.messages[self.last_read+1:]
+
+
 class ChannelScraper(object):
-    
-    def __init__(self, strategy, slack_channel, since=None, interval=30):
-        self.strategy = strategy
+
+    def __init__(self, slack_channel, strategies=None, interval=30, callbacks=None, since=None):
         self.slack_channel = slack_channel
+        self.strategies = strategies if type(strategies) is list else [strategies]
+        self.messages = ScoredMessages(callbacks=callbacks)
         self.last_timestamp = since if since else time.time()
         self.interval = interval
 
     def scrape(self):
-        logger.info(u'Begin scrapping {}'.format(self.slack_channel))
+        logger.info(u'Begin scraping {}'.format(self.slack_channel))
+
         if time.time() - self.last_timestamp < self.interval:
             return
 
-        history = slack_channel.history(since=self.last_timestamp)
+        messages = slack_channel.history(since=self.last_timestamp)['messages']
+        self.messages.extend(reversed(list(messages)))
+
         self.last_timestamp = time.time()
 
-        self.strategy.scan(reversed(history['messages']))
-        logger.info(u'Finished scrapping {}'.format(self.slack_channel))
+        for strategy in self.strategies:
+            strategy.scan(self.messages)
+        logger.info(u'Finished scraping {}'.format(self.slack_channel))
+
+        logger.info(u'ScoredMessages:\n{}'.format(self.messages))
 
 class Strategy(object):
 
     def __init__(self):
         self.callbacks = []
 
-    def register_callback(self, callback):
-        self.callbacks.append(callback)
-
-    def deregister_callback(self, callback):
-        self.callbacks.remove(callback)
-    
-    def _trigger(self, text):
-        for cb in self.callbacks:
-            cb(text)
-
     def scan(self, messages):
         pass
 
 class NaturalBreaksStrategy(Strategy):
 
-    def __init__(self, max_cache_length=500, break_length=5*60):
+    def __init__(self, break_length=10*60):
         super(NaturalBreaksStrategy, self).__init__()
-        self.message_cache = []
-        self.max_cache_length = max_cache_length
         self.break_length = break_length
 
     def __str__(self):
         return 'NaturalBreaksStrategy'
 
     def scan(self, messages):
-        if type(messages) is not list:
-            messages = list(messages)
         logger.info(u'{}: Begin scanning'.format(self))
+        messages = messages.getCurrentBatch()
 
-        if not self.message_cache:
-            last_ts = float(messages[0]['ts'])
-        else:
-            last_ts = float(self.message_cache[-1]['ts'])
+        last_ts = float(messages[0].msg['ts'])
 
-        self.message_cache.append(messages)
-        if len(self.message_cache) > self.max_cache_length:
-            self.message_cache = self.message_cache[-self.max_cache_length:]
-        
         for msg in messages:
-            ts = float(msg['ts'])
+            ts = float(msg.msg['ts'])
             if ts - last_ts > self.break_length:
-                logger.info(u'{}: Found natural break: {}'.format(self, msg['text']))
-                logger.debug(u'{}: Triggering callbacks'.format(self))
-                self._trigger(msg)
-                logger.debug(u'{}: Finished triggering callbacks'.format(self))
+                logger.info(u'{}: Found natural break: {}'.format(self, msg.msg['text']))
+                msg.upvote()
             last_ts = ts
 
         logger.info(u'{}: Finished scanning'.format(self))
@@ -137,15 +171,21 @@ class SlackNotificationCallbackFactory(object):
             sc.api_call("chat.postMessage", channel=dest_channel_id, text=text)
         return _callback
 
-
-cb = SlackNotificationCallbackFactory.getCallback('D1EPGDKGB', 'C0H0TG8CV', 'tower_api_internal')
-
-strategy = NaturalBreaksStrategy()
-strategy.register_callback(cb)
+def noop_callback(msg):
+    pass
 
 #slack_channel = SlackChannel('C0H0TG8CV', 'tower_api_internal')
 slack_channel = SlackChannel('C0F7S8BT5', 'ship_it')
 #slack_channel = SlackChannel('C0SNM2FM4', 'test_notifications2')
 
-scraper = ChannelScraper(strategy, slack_channel, since=time.time() - 10*60*60)
+strategy = NaturalBreaksStrategy()
+
+#cb = SlackNotificationCallbackFactory.getCallback('D1EPGDKGB', 'C0H0TG8CV', 'tower_api_internal')
+cb = noop_callback
+start_time = time.time() - 10 * 60 * 60
+
+scraper = ChannelScraper(slack_channel,
+                         strategies=strategy,
+                         callbacks=cb,
+                         since=start_time)
 scraper.scrape()
